8cf9d7c49f2b22f3e86389f22d4ffeef5cc6c41b
Florian Grandjean <florian.pdf@gmail.com>
2016-11-14 14:11:36 +0100

    add serveur chouettes hiboux ==> serveur_connection

--- serveur_connexion.sh
+++ serveur_connexion.sh
@@ -1,13 +1,21 @@
 read -r -p "Choisi un serveur: 
 -- Hopital 
--- Fontainebleau (bleau)
+-- Bleau_vps (bvps)
+-- Bleau_o2switch (bo2s)
+-- Chouettes
 ==> " response
 case $response in
     [hH][oO][pP][iI][tT][aA][lL]) 
         ssh root@80.67.190.170
         ;;
-    [bB][lL][eE][aA][uU])
+    [b][v][p][s])
         ssh root@80.67.190.137
+        ;;
+    [b][o][2][s])
+        ssh wcsfont@wcs-fontainebleau.fr
+        ;;
+    [c][h][o][u][e][t][t][e][s])
+        ssh chouettes@chouetteshiboux.com
         ;;
     *)
         echo "Pas de serveur portant ce nom"
